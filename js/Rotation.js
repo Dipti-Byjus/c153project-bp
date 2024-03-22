5c421254a4d5f5e4a4137fd3cf503a046494474f
Dipti-Byjus <100108871+Dipti-Byjus@users.noreply.github.com>
2024-03-22 06:10:35 +0530

    Add files via upload

---
+++ js/Rotation.js
@@ -0,0 +1,24 @@
+//Diver rotation component
+AFRAME.registerComponent("diver-rotation-reader", {
+  schema: {
+    speedOfRoation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+
+      //get the data from the attributes
+      this.data.speedOfRoation = this.el.getAttribute("rotation");      
+      this.data.speedOfAscent = this.el.getAttribute("position");
+
+      var diverRotation = this.data.speedOfRoation;      
+      var diverPosition = this.data.speedOfAscent;
+
+      //control the attributes with the Arrow Keys
+     
+      
+      
+     
+    });
+  }
+});
